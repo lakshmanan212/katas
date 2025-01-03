47f53663b2b1aae4113686f369767fbd59520578
Lakshmanan <lakshmanan.palaniyappan.official@gmail.com>
2025-01-03 14:54:50 +0530

    GameLogic added.

---
+++ TicTacToeApp/src/main/java/org/example/Core/GameLogic.java
@@ -0,0 +1,34 @@
+package org.example.Core;
+
+public class GameLogic {
+    private final GameBoard board;
+
+    public GameLogic(GameBoard board) {
+        this.board = board;
+    }
+
+    public boolean isWinningMove(char player) {
+        for (int i = 0; i < Constants.SIZE; i++) {
+            if (isRowWin(i, player) || isColumnWin(i, player)) {
+                return true;
+            }
+        }
+        return isDiagonalWin(player);
+    }
+
+    private boolean isRowWin(int row, char player) {
+        char[][] b = board.getBoard();
+        return b[row][0] == player && b[row][1] == player && b[row][2] == player;
+    }
+
+    private boolean isColumnWin(int col, char player) {
+        char[][] b = board.getBoard();
+        return b[0][col] == player && b[1][col] == player && b[2][col] == player;
+    }
+
+    private boolean isDiagonalWin(char player) {
+        char[][] b = board.getBoard();
+        return (b[0][0] == player && b[1][1] == player && b[2][2] == player) ||
+                (b[0][2] == player && b[1][1] == player && b[2][0] == player);
+    }
+}
