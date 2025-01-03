0dc76987a6b45c568f79208b1bb58c869a207cc9
Lakshmanan <lakshmanan.palaniyappan.official@gmail.com>
2025-01-03 14:51:42 +0530

    GameBoard Added which is having board logic.

---
+++ TicTacToeApp/src/main/java/org/example/TicTacToeApplication.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import org.example.Core.TicTacToe;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicTacToeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicTacToeApplication.class, args);
+		TicTacToe game = new TicTacToe();
+		game.startGame();
+	}
+
+}
