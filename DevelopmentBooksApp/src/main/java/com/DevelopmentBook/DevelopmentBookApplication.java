27bd4de2e90e8582b50d433eff7eeddcadaf05d4
Lakshmanan <lakshmanan.palaniyappan.official@gmail.com>
2025-01-06 19:34:14 +0530

    Project Structure added for development project

---
+++ DevelopmentBooksApp/src/main/java/com/DevelopmentBook/DevelopmentBookApplication.java
@@ -0,0 +1,13 @@
+package com.DevelopmentBook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevelopmentBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevelopmentBookApplication.class, args);
+	}
+
+}
